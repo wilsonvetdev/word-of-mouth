5ab8940d0adebfa98de53f90090ccbe648024bd8
wilsonng21 <wilsongch92@gmail.com>
2020-09-05 15:24:05 -0400

    login sends user to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,15 @@ class UsersController < ApplicationController
     end
 
     def handle_login
-        
         @user = User.find_by(username: params[:username])
-
+        # Getting params[:username] value from the user get request
+        # Handle the request by finding the user in database first, username is unique**
+        # if user exist(truthy) and password pass authentication
+        if @user && @user.authenticate(params[:password])
+            redirect_to user_path(@user)
+        else
+            redirect_to login_path
+        end
     end
 
     def index 
