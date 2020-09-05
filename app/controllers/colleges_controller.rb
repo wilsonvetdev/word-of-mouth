71e4475469e08c4b5945e8c43f9ee1814d517659
wilsonng21 <wilsongch92@gmail.com>
2020-09-05 13:55:22 -0400

    search-colleges works

--- app/controllers/colleges_controller.rb
+++ app/controllers/colleges_controller.rb
@@ -1,2 +1,17 @@
 class CollegesController < ApplicationController
+
+    def index 
+        @colleges = College.search(params[:search]) 
+    end
+
+    def show 
+        @colleges = College.find(params[:id])
+    end
+
+    private 
+
+    def college_params
+        params.require(:college).permit(:name, :city, :state, :search)
+    end
+
 end
